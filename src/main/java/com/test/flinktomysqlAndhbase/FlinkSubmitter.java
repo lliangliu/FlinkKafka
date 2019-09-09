301da1e80a8e6da4bf78e3221058e6de65aba246
lliangliu <806023983@qq.com>
2019-09-09 15:19:01 +0800

    1.改善代码

--- src/main/java/com/test/flinktomysqlAndhbase/FlinkSubmitter.java
+++ src/main/java/com/test/flinktomysqlAndhbase/FlinkSubmitter.java
@@ -1,5 +1,6 @@
 package com.test.flinktomysqlAndhbase;
 import com.alibaba.fastjson.JSON;
+import com.test.Utils.PropertyUtil;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -7,18 +8,23 @@ import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
 import java.util.Properties;
 
 public class FlinkSubmitter {
+    private static String zkconnect= PropertyUtil.getProperty("kafka.zookeeper.connect");
+    private static String kafkaservers=PropertyUtil.getProperty("kafka.bootstrap.servers");
+    private static String keydeserializer=PropertyUtil.getProperty("kafka.key.serializer");
+    private static String valuedeserializer=PropertyUtil.getProperty("kafka.value.serializer");
+    private static String topicName=PropertyUtil.getProperty("kafka_flink_mysql");
     public static void main(String[] args) throws Exception{
         //获取运行时环境
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         //kafka配置文件
         Properties props = new Properties();
-        props.put("bootstrap.servers", "node01:9092");
-        props.put("zookeeper.connect","node01:2181");
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");  //key 反序列化
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer"); //value 反序列化
+        props.put("bootstrap.servers", kafkaservers);
+        props.put("zookeeper.connect",zkconnect);
+        props.put("key.deserializer", keydeserializer);  //key 反序列化
+        props.put("value.deserializer",valuedeserializer ); //value 反序列化
         //这里我们使用的是011版本，011 与 09 或者10 的区别在于，011支持Exactly-once语义
         SingleOutputStreamOperator<Entity> StreamRecord = env.addSource(new FlinkKafkaConsumer010<>(
-                "kafka_flink_mysql",
+                topicName,
                 new SimpleStringSchema(),//String 序列
                 props)).map(string -> JSON.parseObject(string, Entity.class)).setParallelism(1);
         //写入本地
